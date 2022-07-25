c88644d95c5167ca73a172799cbd6810628b1725
Louie-03 <dhdustnr0134@naver.com>
2022-07-25 13:38:05 +0900

    init : 프로젝트 초기 환경 설정

---
+++ src/main/java/louie/hanse/shareplate/SharePlateApplication.java
@@ -0,0 +1,13 @@
+package louie.hanse.shareplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePlateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/louie/hanse/shareplate/SharePlateApplicationTests.java
@@ -0,0 +1,13 @@
+package louie.hanse.shareplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharePlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
