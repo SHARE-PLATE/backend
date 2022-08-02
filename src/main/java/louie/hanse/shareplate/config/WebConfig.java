9452698a8c73dafe1805d153f021ce0f6f9a67ec
Louie-03 <dhdustnr0134@naver.com>
2022-08-02 17:44:40 +0900

    feat : 대소문자 구분 없이 ShareType을 입력 받기 위한 Converter 구현
    스프링 환경 설정에 StringToShareTypeConverter 추가

--- src/main/java/louie/hanse/shareplate/config/WebConfig.java
+++ src/main/java/louie/hanse/shareplate/config/WebConfig.java
@@ -1,9 +1,13 @@
 package louie.hanse.shareplate.config;
 
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
+import louie.hanse.shareplate.converter.StringToShareTypeConverter;
 import louie.hanse.shareplate.interceptor.LoginInterceptor;
 import louie.hanse.shareplate.jwt.JwtProvider;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,7 +20,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtProvider))
-            .order(1)
-            .addPathPatterns("/members", "/members/location");
+                .order(1)
+                .addPathPatterns("/members", "/members/location", "/shares");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd/HH:mm"));
+        registrar.registerFormatters(registry);
+
+        registry.addConverter(new StringToShareTypeConverter());
     }
 }
