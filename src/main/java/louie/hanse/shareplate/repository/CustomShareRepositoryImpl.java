f0f3fcdd6dffb55388965f63f0d0c4cb5c347027
Louie-03 <dhdustnr0134@naver.com>
2022-08-08 16:14:35 +0900

    refactor : 쉐어 검색 시 좌표 계산 로직 도메인으로 분리

--- src/main/java/louie/hanse/shareplate/repository/CustomShareRepositoryImpl.java
+++ src/main/java/louie/hanse/shareplate/repository/CustomShareRepositoryImpl.java
@@ -21,12 +21,28 @@ public class CustomShareRepositoryImpl implements CustomShareRepository {
         return queryFactory
             .selectFrom(share)
             .where(
+                titleContains(request.getKeyword()),
                 share.type.eq(request.getType()).and(
-                    share.latitude.between(member.getLatitude() - 2, member.getLatitude() + 2).and(
-                        share.longitude.between(member.getLongitude() - 2, member.getLongitude() + 2))),
-                titleContains(request.getKeyword())
+                    share.latitude.between(
+                        member.calculateStartLatitude(),
+                        member.calculateEndLatitude()).and(
+                        share.longitude.between(
+                            member.calculateStartLongitude(),
+                            member.calculateEndLongitude())))
             ).fetch();
     }
+
+//    @Override
+//    public List<Share> searchAroundMember(Member member, ShareSearchRequest request) {
+//        return queryFactory
+//            .selectFrom(share)
+//            .where(
+//                share.type.eq(request.getType()).and(
+//                    share.latitude.between(member.getLatitude() - 2, member.getLatitude() + 2).and(
+//                        share.longitude.between(member.getLongitude() - 2, member.getLongitude() + 2))),
+//                titleContains(request.getKeyword())
+//            ).fetch();
+//    }
 
     private BooleanExpression titleContains(String keyword) {
         return StringUtils.hasText(keyword) ? share.title.contains(keyword) : null;
