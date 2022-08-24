e31ce28b4d292ff85774a5f008ccd9d5e05941e6
Louie-03 <dhdustnr0134@naver.com>
2022-08-24 17:43:27 +0900

    feat : 요구사항에 맞게 내 쉐어 기능 변경
    - 조회하고 싶은 쉐어 타입과 마감 여부를 파라미터로 받아서 결과를 반환하도록 변경

--- src/main/java/louie/hanse/shareplate/repository/CustomShareRepository.java
+++ src/main/java/louie/hanse/shareplate/repository/CustomShareRepository.java
@@ -1,7 +1,9 @@
 package louie.hanse.shareplate.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import louie.hanse.shareplate.domain.Share;
+import louie.hanse.shareplate.type.ShareType;
 import louie.hanse.shareplate.web.dto.share.ShareCommonResponse;
 import louie.hanse.shareplate.web.dto.share.ShareRecommendationRequest;
 import louie.hanse.shareplate.web.dto.share.ShareSearchRequest;
@@ -12,4 +14,14 @@ public interface CustomShareRepository {
 
     List<ShareCommonResponse> recommendationAroundMember(ShareRecommendationRequest request);
 
+    List<Share> findByWriterIdAndTypeAndIsExpired(
+        Long writerId, ShareType type, boolean expired, LocalDateTime currentDateTime);
+
+    List<Share> findWithEntryByMemberIdAndTypeAndIsExpired(
+        Long memberId, ShareType type, boolean expired, LocalDateTime currentDateTime);
+
+    List<Share> findWithWishByMemberIdAndTypeAndIsExpired(
+        Long memberId, ShareType type, boolean expired, LocalDateTime currentDateTime);
+
 }
+
--- src/main/java/louie/hanse/shareplate/repository/ShareRepository.java
+++ src/main/java/louie/hanse/shareplate/repository/ShareRepository.java
@@ -1,22 +1,9 @@
 package louie.hanse.shareplate.repository;
 
-import java.util.List;
 import louie.hanse.shareplate.domain.Share;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ShareRepository extends JpaRepository<Share, Long>, CustomShareRepository {
-
-    List<Share> findByWriterId(Long writerId);
-
-    @Query("select s from Share s "
-        + "join s.entries e on e.member.id = :memberId ")
-    List<Share> findWithEntry(@Param("memberId") Long memberId);
-
-    @Query("select s from Share s "
-        + "join s.wishList w on w.member.id = :memberId ")
-    List<Share> findWithWish(@Param("memberId") Long memberId);
 
     boolean existsByIdAndWriterId(Long id, Long writerId);
 
