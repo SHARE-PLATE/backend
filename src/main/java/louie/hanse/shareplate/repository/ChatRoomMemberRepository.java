fa92c8f7691af5991b9cd23c4eec2a900b52f3df
Louie-03 <dhdustnr0134@naver.com>
2022-08-25 14:31:09 +0900

    feat : 채팅방 나가기 기능 구현

--- src/main/java/louie/hanse/shareplate/repository/ChatRoomMemberRepository.java
+++ src/main/java/louie/hanse/shareplate/repository/ChatRoomMemberRepository.java
@@ -2,7 +2,18 @@ package louie.hanse.shareplate.repository;
 
 import louie.hanse.shareplate.domain.ChatRoomMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
 
+    @Query("select crf from ChatRoomMember crf "
+        + "join fetch crf.chatRoom cr "
+        + "join fetch cr.share s "
+        + "where crf.chatRoom.id = :chatRoomId and "
+        + "crf.member.id = :memberId")
+    ChatRoomMember findWithShareByChatRoomIdAndMemberId(
+        @Param("chatRoomId") Long chatRoomId, @Param("memberId") Long memberId);
+
+    void deleteByChatRoomIdAndMemberId(Long chatRoomId, Long memberId);
 }
