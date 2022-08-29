c068e491fbbaa249e5ea6e726a6942d366873d55
Louie-03 <dhdustnr0134@naver.com>
2022-08-29 16:32:40 +0900

    feat : 키워드 등록 기능 구현

---
+++ src/main/java/louie/hanse/shareplate/service/KeywordService.java
@@ -0,0 +1,25 @@
+package louie.hanse.shareplate.service;
+
+import lombok.RequiredArgsConstructor;
+import louie.hanse.shareplate.domain.Keyword;
+import louie.hanse.shareplate.domain.Member;
+import louie.hanse.shareplate.repository.KeywordRepository;
+import louie.hanse.shareplate.web.dto.keyword.KeywordRegisterRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class KeywordService {
+
+    private final KeywordRepository keywordRepository;
+    private final MemberService memberService;
+
+    @Transactional
+    public void register(KeywordRegisterRequest request, Long memberId) {
+        Member member = memberService.findByIdOrElseThrow(memberId);
+        Keyword keyword = request.toEntity(member);
+        keywordRepository.save(keyword);
+    }
+}
