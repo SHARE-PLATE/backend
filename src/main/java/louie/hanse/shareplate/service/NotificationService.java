9b1d4ddc083d0901f1bb7bce03c820a104020a0a
rkolx <olx.rko.o@gmail.com>
2022-08-31 17:59:48 +0900

    feat : 활동 알림 기능 구현
    - 키워드 알림에 memberId 추가(자신 제외)

--- src/main/java/louie/hanse/shareplate/service/NotificationService.java
+++ src/main/java/louie/hanse/shareplate/service/NotificationService.java
@@ -5,11 +5,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import louie.hanse.shareplate.domain.ActivityNotification;
+import louie.hanse.shareplate.domain.Entry;
 import louie.hanse.shareplate.domain.Keyword;
+import louie.hanse.shareplate.domain.Member;
 import louie.hanse.shareplate.domain.Notification;
 import louie.hanse.shareplate.domain.Share;
+import louie.hanse.shareplate.repository.EntryRepository;
 import louie.hanse.shareplate.repository.KeywordRepository;
 import louie.hanse.shareplate.repository.NotificationRepository;
+import louie.hanse.shareplate.type.ActivityType;
 import louie.hanse.shareplate.type.NotificationType;
 import louie.hanse.shareplate.web.dto.notification.ActivityNotificationResponse;
 import louie.hanse.shareplate.web.dto.notification.KeywordNotificationResponse;
@@ -23,8 +27,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationService {
 
     private final ShareService shareService;
+    private final MemberService memberService;
     private final NotificationRepository notificationRepository;
     private final KeywordRepository keywordRepository;
+    private final EntryRepository entryRepository;
     private final MessageSendingOperations messageSendingOperations;
 
     public List<ActivityNotificationResponse> getActivityNotificationList(Long memberId) {
@@ -56,12 +62,12 @@ public class NotificationService {
     }
 
     @Transactional
-    public void registerAndSend(Long shareId) {
+    public void saveKeywordNotificationAndSend(Long shareId, Long memberId) {
         Share share = shareService.findByIdOrElseThrow(shareId);
         double longitude = share.getLongitude();
         double latitude = share.getLatitude();
         List<Keyword> keywords = keywordRepository.findAllByContainsContentsAndAroundShareV2(
-            share.getTitle(), longitude - 2, longitude + 2,
+            memberId, share.getTitle(), longitude - 2, longitude + 2,
             latitude - 2, latitude + 2);
 
         List<Notification> notifications = new ArrayList<>();
@@ -78,6 +84,30 @@ public class NotificationService {
             Long keywordId = keywords.get(i).getId();
             messageSendingOperations.convertAndSend(
                 "/queue/notifications/keywords/" + keywordId, responses.get(i));
+        }
+    }
+
+    @Transactional
+    public void saveActivityNotificationAndSend(Long shareId, Long memberId) {
+        Share share = shareService.findByIdOrElseThrow(shareId);
+        Member member = memberService.findByIdOrElseThrow(memberId);
+        List<Entry> entries = entryRepository.findAllByShareIdAndMemberId(shareId,
+            memberId);
+
+        List<ActivityNotification> activityNotifications = new ArrayList<>();
+        List<ActivityNotificationResponse> responses = new ArrayList<>();
+        for (Entry entry : entries) {
+            ActivityNotification activityNotification = new ActivityNotification(share,
+                entry.getMember(), NotificationType.ACTIVITY, ActivityType.ENTRY, member);
+            activityNotifications.add(activityNotification);
+            responses.add(new ActivityNotificationResponse(activityNotification));
+        }
+        notificationRepository.saveAll(activityNotifications);
+
+        for (int i = 0; i < entries.size(); i++) {
+            Long entryMemberId = entries.get(i).getMember().getId();
+            messageSendingOperations.convertAndSend(
+                "/queue/notifications/entries/" + entryMemberId, responses.get(i));
         }
     }
 }
