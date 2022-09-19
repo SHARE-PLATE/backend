da317264f3909c2c41eef31ac956b6c36fc57c22
rkolx <olx.rko.o@gmail.com>
2022-09-19 15:42:09 +0900

    refactor : 알림 기능 관련 실패케이스 테스트 분리
    - 단건삭제, 선택삭제로 테스트 클래스 분리
    - NotificationExceptionType : 타입 메세지 수정

--- src/main/java/louie/hanse/shareplate/exception/type/NotificationExceptionType.java
+++ src/main/java/louie/hanse/shareplate/exception/type/NotificationExceptionType.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 
 public enum NotificationExceptionType implements ExceptionType {
 
-    NOTIFICATION_ID_IS_NEGATIVE("NOTIFICATION001", "알림 id는 양수여야 합니다. 요청한 알림 id : {id}", BAD_REQUEST),
+    NOTIFICATION_ID_IS_NEGATIVE("NOTIFICATION001", "알림 id는 양수여야 합니다.", BAD_REQUEST),
     NOTIFICATION_NOT_FOUND("NOTIFICATION002", "존재하지 않은 알림입니다.", BAD_REQUEST),
     OTHER_MEMBER_CAN_NOT_DELETE_NOTIFICATION("NOTIFICATION003", "다른 사용자의 알림을 삭제할 수 없습니다.", FORBIDDEN),
     EMPTY_NOTIFICATION_INFO("NOTIFICATION004", "요청한 알림정보 필드값이 비어있습니다.", BAD_REQUEST);
