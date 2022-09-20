f2718ff412c81ee8b5133d75f2b8320f86552178
Louie-03 <dhdustnr0134@naver.com>
2022-09-20 23:55:34 +0900

    feat : MineType을 제대로 생성하지 못한다면 예외를 반환하도록 구현

--- src/main/java/louie/hanse/shareplate/type/MineType.java
+++ src/main/java/louie/hanse/shareplate/type/MineType.java
@@ -1,11 +1,19 @@
 package louie.hanse.shareplate.type;
 
+import louie.hanse.shareplate.exception.GlobalException;
+import louie.hanse.shareplate.exception.type.ShareExceptionType;
+
 public enum MineType {
     ENTRY, WRITER, WISH;
 
     public static MineType valueOfWithCaseInsensitive(String name) {
         name = name.toUpperCase();
-        return valueOf(name);
+
+        try {
+            return valueOf(name);
+        } catch (IllegalArgumentException e) {
+            throw new GlobalException(ShareExceptionType.INCORRECT_MINE_VALUE);
+        }
     }
 
     public boolean isEntry() {
