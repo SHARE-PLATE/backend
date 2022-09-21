441126db5898a02b4e5c106176d5febaa3121103
Louie-03 <dhdustnr0134@naver.com>
2022-09-21 16:03:15 +0900

    feat : 쉐어 편집 API 요청값 검증 구현

--- src/main/java/louie/hanse/shareplate/web/dto/share/ShareEditRequest.java
+++ src/main/java/louie/hanse/shareplate/web/dto/share/ShareEditRequest.java
@@ -2,29 +2,73 @@ package louie.hanse.shareplate.web.dto.share;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import lombok.Setter;
 import louie.hanse.shareplate.domain.Member;
 import louie.hanse.shareplate.domain.Share;
 import louie.hanse.shareplate.type.ShareType;
+import louie.hanse.shareplate.validator.ValidLatitude;
+import louie.hanse.shareplate.validator.ValidLongitude;
+import louie.hanse.shareplate.validator.ValidShareImage;
 import org.springframework.web.multipart.MultipartFile;
 
 @Setter
 public class ShareEditRequest {
 
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
     private ShareType type;
-    private List<MultipartFile> images;
+
+    @NotEmpty(message = "요청한 쉐어정보 값이 비어있습니다.")
+    @Size(max = 5, message = "이미지 5개를 초과하였습니다.")
+    private List<@Valid @NotNull @ValidShareImage MultipartFile> images;
+
+    @NotEmpty(message = "요청한 쉐어정보 값이 비어있습니다.")
     private String title;
-    private int price;
-    private int originalPrice;
-    private int recruitment;
-    private String locationGuide;
-    private boolean locationNegotiation;
-    private boolean priceNegotiation;
+
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    @Positive(message = "요청값은 양수여야 합니다.")
+    private Integer price;
+
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    @Positive(message = "요청값은 양수여야 합니다.")
+    private Integer originalPrice;
+
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    @Positive(message = "요청값은 양수여야 합니다.")
+    private Integer recruitment;
+
+    @NotEmpty(message = "요청한 쉐어정보 값이 비어있습니다.")
     private String location;
-    private double latitude;
-    private double longitude;
+
+    @NotEmpty(message = "요청한 쉐어정보 값이 비어있습니다.")
+    private String locationGuide;
+
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    private Boolean locationNegotiation;
+
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    private Boolean priceNegotiation;
+
+    private List<@Valid @NotEmpty(message = "요청한 쉐어정보 값이 비어있습니다.") String> hashtags;
+
+    @ValidLatitude
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    private Double latitude;
+
+    @ValidLongitude
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
+    private Double longitude;
+
+    @NotEmpty(message = "요청한 쉐어정보 값이 비어있습니다.")
     private String description;
-    private List<String> hashtags;
+
+    @Future(message = "약속 시간은 현재 시간 이후로 설정해야 합니다.")
+    @NotNull(message = "요청한 쉐어정보 값이 비어있습니다.")
     private LocalDateTime closedDateTime;
 
     public Share toEntity(Long id, Member member) {
