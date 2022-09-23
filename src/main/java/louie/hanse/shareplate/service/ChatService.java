8b4824469240b002f64d9094622cf7ae90a94dc1
Louie-03 <dhdustnr0134@naver.com>
2022-09-23 14:38:00 +0900

    refactor, fix : 새로 작성된 채팅 내용 알림 API 수정, writtenByMe 필드가 무조건 true로 반환되는 문제 해결

--- src/main/java/louie/hanse/shareplate/service/ChatService.java
+++ src/main/java/louie/hanse/shareplate/service/ChatService.java
@@ -3,9 +3,11 @@ package louie.hanse.shareplate.service;
 import lombok.RequiredArgsConstructor;
 import louie.hanse.shareplate.domain.Chat;
 import louie.hanse.shareplate.domain.ChatRoom;
+import louie.hanse.shareplate.domain.ChatRoomMember;
 import louie.hanse.shareplate.domain.Member;
 import louie.hanse.shareplate.repository.ChatRepository;
 import louie.hanse.shareplate.web.dto.chat.ChatDetailResponse;
+import org.springframework.messaging.core.MessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,15 +19,22 @@ public class ChatService {
     private final ChatRepository chatRepository;
     private final ChatRoomService chatRoomService;
     private final MemberService memberService;
+    private final MessageSendingOperations messageSendingOperations;
 
     @Transactional
-    public ChatDetailResponse save(Long chatRoomId, Long memberId, String contents) {
+    public void saveAndResponseChatMessage(Long chatRoomId, Long memberId, String contents) {
         //TODO 추후 커스텀 예외처리 적용
         ChatRoom chatRoom = chatRoomService.findByIdOrElseThrow(chatRoomId);
         Member member = memberService.findByIdOrElseThrow(memberId);
         Chat chat = new Chat(chatRoom, member, contents);
         chatRepository.save(chat);
-        return new ChatDetailResponse(chat, member);
+        for (ChatRoomMember chatRoomMember : chatRoom.getChatRoomMembers()) {
+            ChatDetailResponse chatDetailResponse = new ChatDetailResponse(
+                chat, chatRoomMember.getMember());
+            messageSendingOperations.convertAndSend(
+                "/topic/chatroom-members/" + chatRoomMember.getId(), chatDetailResponse);
+
+        }
     }
 
     public int getTotalUnread(Long memberId) {
