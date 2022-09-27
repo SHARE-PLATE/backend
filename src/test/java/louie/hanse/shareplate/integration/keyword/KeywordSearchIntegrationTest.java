54df3a5ab7ad4dad5073fbe2e1e57b823ca565b3
rkolx <olx.rko.o@gmail.com>
2022-09-27 22:04:30 +0900

    test : keyword 조회 실패케이스 추가
    - 유효하지 않은 회원이 요청할 때

--- src/test/java/louie/hanse/shareplate/integration/keyword/KeywordSearchIntegrationTest.java
+++ src/test/java/louie/hanse/shareplate/integration/keyword/KeywordSearchIntegrationTest.java
@@ -1,10 +1,12 @@
 package louie.hanse.shareplate.integration.keyword;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 import io.restassured.http.ContentType;
+import louie.hanse.shareplate.exception.type.MemberExceptionType;
 import louie.hanse.shareplate.integration.InitIntegrationTest;
 import louie.hanse.shareplate.jwt.JwtProvider;
 import org.junit.jupiter.api.DisplayName;
@@ -35,4 +37,20 @@ class KeywordSearchIntegrationTest extends InitIntegrationTest {
             .body("", hasSize(2));
     }
 
+    @Test
+    void 유효하지_않은_회원이_키워드를_조회한다() {
+        String accessToken = jwtProvider.createAccessToken(1L);
+
+        given(documentationSpec)
+            .header(AUTHORIZATION, accessToken)
+            .accept(ContentType.JSON)
+
+            .when()
+            .get("/keywords")
+
+            .then()
+            .statusCode(MemberExceptionType.MEMBER_NOT_FOUND.getStatusCode().value())
+            .body("errorCode", equalTo(MemberExceptionType.MEMBER_NOT_FOUND.getErrorCode()))
+            .body("message", equalTo(MemberExceptionType.MEMBER_NOT_FOUND.getMessage()));
+    }
 }
