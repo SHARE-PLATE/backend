2d1ff1832d4ca9f35dccfc7e10ab10662b0a95cf
Louie-03 <dhdustnr0134@naver.com>
2022-10-05 20:56:33 +0900

    refactor : 대부분의 통합 테스트에서 사용하는 JwtProvider 필드를 InitIntegrationTest로 변경

--- src/test/java/louie/hanse/shareplate/integration/entry/EntryJoinIntegrationTest.java
+++ src/test/java/louie/hanse/shareplate/integration/entry/EntryJoinIntegrationTest.java
@@ -13,7 +13,6 @@ import louie.hanse.shareplate.exception.type.EntryExceptionType;
 import louie.hanse.shareplate.exception.type.MemberExceptionType;
 import louie.hanse.shareplate.exception.type.ShareExceptionType;
 import louie.hanse.shareplate.integration.InitIntegrationTest;
-import louie.hanse.shareplate.jwt.JwtProvider;
 import louie.hanse.shareplate.service.ShareService;
 import louie.hanse.shareplate.web.dto.share.ShareRegisterRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -23,9 +22,6 @@ import org.springframework.http.HttpStatus;
 
 @DisplayName("쉐어 참여 기능 통합테스트")
 class EntryJoinIntegrationTest extends InitIntegrationTest {
-
-    @Autowired
-    JwtProvider jwtProvider;
 
     @Autowired
     ShareService shareService;
