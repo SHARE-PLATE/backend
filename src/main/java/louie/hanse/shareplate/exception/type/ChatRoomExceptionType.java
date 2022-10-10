e1ece74e9f4e144309df6cd39a850312ee468f9d
rkolx <olx.rko.o@gmail.com>
2022-10-10 17:55:14 +0900

    feat : 일대일 채팅 실패케이스 추가 로직 구현
    - ChatRoomExceptionType : WRITER_CAN_NOT_QUESTION_CHAT 예외 타입 추가
    - ChatRoomService : 자신이 등록한 쉐어에 1:1채팅을 요청할 경우

--- src/main/java/louie/hanse/shareplate/exception/type/ChatRoomExceptionType.java
+++ src/main/java/louie/hanse/shareplate/exception/type/ChatRoomExceptionType.java
@@ -10,7 +10,8 @@ public enum ChatRoomExceptionType implements ExceptionType {
     PATH_VARIABLE_EMPTY_CHATROOM_ID("CHATROOM003", "PathVariable의 ChatroomId가 비어있습니다.", BAD_REQUEST),
     CHATROOM_ID_IS_NEGATIVE("CHATROOM004", "채팅방 id는 양수여야 합니다.", BAD_REQUEST),
     CHAT_ROOM_NOT_FOUND("CHATROOM005", "존재하지 않는 채팅방입니다.", BAD_REQUEST),
-    EMPTY_CHATROOM_INFO("CHATROOM007", "요청한 채팅방정보 필드값이 비어있습니다.", BAD_REQUEST);
+    EMPTY_CHATROOM_INFO("CHATROOM007", "요청한 채팅방정보 필드값이 비어있습니다.", BAD_REQUEST),
+    WRITER_CAN_NOT_QUESTION_CHAT("CHATROOM008", "내가 등록한 쉐어에는 일대일 채팅을 할 수 없습니다.", BAD_REQUEST);
 
     private final String errorCode;
     private final String message;
