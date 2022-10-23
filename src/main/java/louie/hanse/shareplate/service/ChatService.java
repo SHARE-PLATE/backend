93ece4bde90cfc2e6599013dee8e08071f9bf480
rkolx <olx.rko.o@gmail.com>
2022-10-23 14:13:20 +0900

    feat : 문의 채팅방 알림 구현
    - 문의채팅방 생성 후 채팅을 보내야지만 글쓴이가 추가되도록 로직 변경

--- src/main/java/louie/hanse/shareplate/service/ChatService.java
+++ src/main/java/louie/hanse/shareplate/service/ChatService.java
@@ -1,12 +1,20 @@
 package louie.hanse.shareplate.service;
 
 import lombok.RequiredArgsConstructor;
+import louie.hanse.shareplate.domain.ActivityNotification;
 import louie.hanse.shareplate.domain.Chat;
 import louie.hanse.shareplate.domain.ChatRoom;
 import louie.hanse.shareplate.domain.ChatRoomMember;
+import louie.hanse.shareplate.domain.Entry;
 import louie.hanse.shareplate.domain.Member;
+import louie.hanse.shareplate.domain.Share;
 import louie.hanse.shareplate.repository.ChatRepository;
+import louie.hanse.shareplate.repository.EntryRepository;
+import louie.hanse.shareplate.repository.NotificationRepository;
+import louie.hanse.shareplate.type.ActivityType;
+import louie.hanse.shareplate.type.NotificationType;
 import louie.hanse.shareplate.web.dto.chat.ChatDetailResponse;
+import louie.hanse.shareplate.web.dto.notification.ActivityNotificationResponse;
 import org.springframework.messaging.core.MessageSendingOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ChatService {
 
-    private final ChatRepository chatRepository;
     private final ChatRoomService chatRoomService;
     private final MemberService memberService;
+    private final ChatRepository chatRepository;
+    private final EntryRepository entryRepository;
+    private final NotificationRepository notificationRepository;
     private final MessageSendingOperations messageSendingOperations;
 
     @Transactional
@@ -26,11 +36,25 @@ public class ChatService {
         //TODO 추후 커스텀 예외처리 적용
         ChatRoom chatRoom = chatRoomService.findByIdOrElseThrow(chatRoomId);
         Member member = memberService.findByIdOrElseThrow(memberId);
+        Share share = chatRoom.getShare();
+        if (chatRoom.isQuestion() && chatRoom.getChats().isEmpty()) {
+            Member shareWriter = share.getWriter();
+            chatRoom.addChatRoomMember(shareWriter);
+            ActivityNotification activityNotification = new ActivityNotification(
+                share, member, NotificationType.ACTIVITY, ActivityType.QUESTION_CHATROOM, member);
+            notificationRepository.save(activityNotification);
+
+            Entry entry = entryRepository.findByShareIdAndMemberId(share.getId(),
+                shareWriter.getId());
+            messageSendingOperations.convertAndSend(
+                "/queue/notifications/entries/" + entry.getId(),
+                new ActivityNotificationResponse(activityNotification));
+        }
         Chat chat = new Chat(chatRoom, member, contents);
         chatRepository.save(chat);
         for (ChatRoomMember chatRoomMember : chatRoom.getChatRoomMembers()) {
             ChatDetailResponse chatDetailResponse = new ChatDetailResponse(
-                chat, chatRoomMember.getMember(), chatRoom.getShare());
+                chat, chatRoomMember.getMember(), share);
             messageSendingOperations.convertAndSend(
                 "/topic/chatroom-members/" + chatRoomMember.getId(), chatDetailResponse);
 
